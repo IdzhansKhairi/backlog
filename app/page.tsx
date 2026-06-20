9691b567c7ca5708e338e1e2540550940d3348a8
Idzhans Khairi <muhammad.idzhans@enfrasys.com>
2026-06-21 00:28:17 +0800

    Change app description and make base url redirect to the login page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  // Redirect the root path (/) to /dashboard
-  redirect("/dashboard");
+  // Redirect the root path (/) to /login
+  redirect("/login");
 }
